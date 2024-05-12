6a1979a9852e566032109ea87d63554253ac5468
lordik <jetlord@mail.ru>
2024-05-12 22:19:37 +0300

    dz_5

--- les_5/ПРАКТИКА/trading_economics/run.py
+++ les_5/ПРАКТИКА/trading_economics/run.py
@@ -1,16 +1,14 @@
-''' Файл для запуска Scrapy '''
-
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.reactor import install_reactor
-from scrapy.utils.log import configure_logging 
+from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
-from ligaparser.spiders.liga_pro import LigaProSpider
+from spiders.countries import CountriesSpider
 
 
 if __name__ == "__main__":
     configure_logging()
     install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
     process = CrawlerProcess(get_project_settings())
-    process.crawl(LigaProSpider)
+    process.crawl(CountriesSpider)
     process.start()
